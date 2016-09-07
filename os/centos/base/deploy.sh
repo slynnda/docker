034a74cb558077661d598df6e7a73935d865856e
Sophia Hudson <sophia.hudson@technicallyspeaking.io>
2016-09-07 03:34:36 -0600

    Committing current progress so I can go to sleep

--- os/centos/base/deploy.sh
+++ os/centos/base/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker build -t technicallyspeaking:latest --label="centos-base" --quiet .
+REPO=technicallyspeaking/dev-envs
+IMAGE_TAG=base-centos-latest
+
+docker build -t $REPO:$IMAGE_TAG . &&
+docker save $REPO:$IMAGE_TAG | docker-squash -t squash -verbose | docker load &&
+docker push $REPO:$IMAGE_TAG
 
